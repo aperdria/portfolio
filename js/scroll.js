3fa01e247d3e3f052b8eaf70f70e8b40aa2e8d77
Amélie Perdriaud <aperdria@users.noreply.github.com>
2021-01-21 19:53:28 +0100

    Fixed scroll menu issue

--- js/scroll.js
+++ js/scroll.js
@@ -1,46 +1,49 @@
+var initialScrollTop = document.documentElement.scrollTop;
 var list = document.getElementsByClassName("menu-item");
+var coordinates = [];
 
-if (list) {
+for (let item of list) {
+    var key = item.getAttribute("href");
+    let elem = document.querySelector(key);
+    let rect = elem.getBoundingClientRect();
+    coordinates[key] = rect;
+}
 
-    var coordinates = [];
-    for (let item of list) {
-        var key = item.getAttribute("href");
-        let elem = document.querySelector(key);
-        let rect = elem.getBoundingClientRect();
-        coordinates[key] = rect;
-    }
+/* Listen to scroll event */
+document.addEventListener('scroll', function () {
+    var currentPosition = document.documentElement.scrollTop;
 
-    /* Scroll to relevant section */
-    var anchor = window.location.hash.substr(1);
-    document.addEventListener('scroll', function (event) {
-        var list = document.getElementsByClassName("menu-item");
-        for (var i = 0; i < list.length; i++) {
-            // If element is the one selected
-            var key = list[i].getAttribute("href");
-            var keyNext = key;
-            if (i < list.length - 1) {
-                keyNext = list[i + 1].getAttribute("href");
-            }
-            if (document.documentElement.scrollTop > coordinates[key].top &&
-                document.documentElement.scrollTop <= coordinates[keyNext].top) {
-                list[i].className = "menu-item active";
-            } else {
-                list[i].className = "menu-item";
-            }
+    for (var i = 0; i < list.length; i++) {
+        var key = list[i].getAttribute("href");
+        var keyNext = key;
+
+        if (i < list.length - 1) {
+            keyNext = list[i + 1].getAttribute("href");
         }
-    }, true);
 
-    function scrollTo(hash) {
-        location.hash = "#" + hash;
+        var elementPositionY = coordinates[key].top + initialScrollTop;
+        var nextElementPositionY = coordinates[keyNext].top + initialScrollTop;
+
+        if (currentPosition + 1 >= elementPositionY &&
+            (currentPosition <= nextElementPositionY - 1 || key === keyNext)) {
+            list[i].className = "menu-item active";
+        } else {
+            list[i].className = "menu-item";
+        }
     }
+}, true);
+
+/* Scroll to relevant anchor */
+function scrollTo(hash) {
+    location.hash = "#" + hash;
 }
 
 /* Vertical progress scroll line indicator */
 $(window).scroll(function () {
-    var wintop = $(window).scrollTop(),
+    var winy = $(window).scrollTop(),
         docheight =
         $(document).height(),
         winheight = $(window).height();
-    var scrolled = (wintop / (docheight - winheight)) * 100;
+    var scrolled = (winy / (docheight - winheight)) * 100;
     $('.scroll-line-vertical').css('height', (scrolled + '%'));
 });
